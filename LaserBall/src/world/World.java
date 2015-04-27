3f03ebffccdd446255e11e1ea218df7ef2b3590e
Zephyres <zephyr.ocn@gmail.com>
2015-04-28 07:42:56 +0800

    Initialized project

---
+++ LaserBall/src/world/World.java
@@ -0,0 +1,38 @@
+package world;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+import java.util.ArrayList;
+import entity.Entity;
+
+public class World extends BasicGameState {
+	ArrayList<Entity> renderList;
+	
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1)
+			throws SlickException {
+		renderList = new ArrayList<Entity>();
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+			throws SlickException {
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+		
+	}
+
+	@Override
+	public int getID() {
+		return 0;
+	}
+
+}
